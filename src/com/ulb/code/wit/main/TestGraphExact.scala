62f5c91ea47d144b272af41ead8745e4be2c5774
Rohit <rohit4phy@gmail.com>
2015-10-08 12:43:34 +0200

    testing

--- src/com/ulb/code/wit/main/TestGraphExact.scala
+++ src/com/ulb/code/wit/main/TestGraphExact.scala
@@ -6,6 +6,7 @@ package com.ulb.code.wit.main
 
 import org.apache.spark._
 import org.apache.spark.graphx._
+import org.apache.spark.graphx.lib._
 import org.apache.spark.rdd.RDD
 import com.ulb.code.wit.util._
 import org.apache.log4j.Logger
@@ -15,6 +16,7 @@ import scala.util.control.Breaks.break
 import scala.collection.mutable.HashMap
 import java.io.{ File, FileWriter, BufferedWriter }
 import java.util.Date
+import scala.reflect.ClassTag
 object TestGraphExact {
 
   val distance = 4
@@ -112,8 +114,8 @@ object TestGraphExact {
 
         } //end of else
 
-        graph = graph.pregel((0, msgs), distance, EdgeDirection.Out)(vertexProgram, sendMessage, messageCombiner)
-        var ver = graph.vertices.collect
+        graph = preg(graph, (0, msgs), distance, EdgeDirection.Out)(vertexProgram, sendMessage, messageCombiner)
+
         nodes = nodes.empty
       } //end of if
 
@@ -180,7 +182,7 @@ object TestGraphExact {
 
       } //end of else
 
-      graph = graph.pregel((0, msgs), distance, EdgeDirection.Out)(vertexProgram, sendMessage, messageCombiner)
+      graph = preg(graph, (0, msgs), distance, EdgeDirection.Out)(vertexProgram, sendMessage, messageCombiner)
 
       nodes = nodes.empty
 
@@ -258,4 +260,49 @@ object TestGraphExact {
 
   }
 
+  def preg[VD: ClassTag, ED: ClassTag, A: ClassTag](graph: Graph[VD, ED],
+                                                    initialMsg: A,
+                                                    maxIterations: Int = Int.MaxValue,
+                                                    activeDirection: EdgeDirection = EdgeDirection.Either)(vprog: (VertexId, VD, A) => VD,
+                                                                                                           sendMsg: EdgeTriplet[VD, ED] => Iterator[(VertexId, A)],
+                                                                                                           mergeMsg: (A, A) => A): Graph[VD, ED] =
+    {
+      var g = graph.mapVertices((vid, vdata) => vprog(vid, vdata, initialMsg)).cache()
+      // compute the messages
+      var messages = g.mapReduceTriplets(sendMsg, mergeMsg)
+      var activeMessages = messages.count()
+      // Loop
+      var prevG: Graph[VD, ED] = null
+      var i = 0
+      while (activeMessages > 0 && i < maxIterations) {
+        // Receive the messages. Vertices that didn't get any messages do not appear in newVerts.
+        val newVerts = g.vertices.innerJoin(messages)(vprog).cache()
+        // Update the graph with the new vertices.
+        prevG = g
+        g = g.outerJoinVertices(newVerts) { (vid, old, newOpt) => newOpt.getOrElse(old) }
+        g.cache()
+        var ver = g.vertices.collect
+        val oldMessages = messages
+        // Send new messages. Vertices that didn't get any messages don't appear in newVerts, so don't
+        // get to send messages. We must cache messages so it can be materialized on the next line,
+        // allowing us to uncache the previous iteration.
+        messages = g.mapReduceTriplets(sendMsg, mergeMsg, Some((newVerts, activeDirection))).cache()
+        // The call to count() materializes `messages`, `newVerts`, and the vertices of `g`. This
+        // hides oldMessages (depended on by newVerts), newVerts (depended on by messages), and the
+        // vertices of prevG (depended on by newVerts, oldMessages, and the vertices of g).
+        activeMessages = messages.count()
+
+        println("Pregel finished iteration " + i)
+
+        // Unpersist the RDDs hidden by newly-materialized RDDs
+        oldMessages.unpersist(blocking = false)
+        newVerts.unpersist(blocking = false)
+        prevG.unpersistVertices(blocking = false)
+        prevG.edges.unpersist(blocking = false)
+        // count the iteration
+        i += 1
+      }
+
+      g
+    }
 }
